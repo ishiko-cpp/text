b0f720ccdd7446f7547c7be8897e117a49b58a7b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-11-01 21:19:43 +0000

    Add files

---
+++ Include/Ishiko/Text.h
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Text/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TEXT_H_
+#define _ISHIKO_TEXT_H_
+
+#include "Text/linkoptions.h"
+
+#endif
---
+++ Include/Ishiko/Text/linkoptions.h
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2016-2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Text/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TEXT_LINKOPTIONS_H_
+#define _ISHIKO_TEXT_LINKOPTIONS_H_
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoText-d.lib")
+#else
+#pragma comment(lib, "IshikoText.lib")
+#endif
+
+#endif
