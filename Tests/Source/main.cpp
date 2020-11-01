8414101ac1ab03362be0a700413b22107ef7c2a5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-11-01 21:42:31 +0000

    Add test file

---
+++ Tests/Source/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/Text/blob/master/LICENSE.txt
+*/
+
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoText");
+
+    return theTestHarness.run();
+}
