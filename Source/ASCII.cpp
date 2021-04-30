e6b3dbd750a52416d9484b47645269dd5aa57201
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-04-30 12:22:55 +0100

    Implement ASCII::Split and ASCII::Trim (#13)

--- Source/ASCII.cpp
+++ Source/ASCII.cpp
@@ -11,6 +11,39 @@ namespace Ishiko
 namespace Text
 {
 
+std::vector<std::string> ASCII::Split(const std::string& str)
+{
+    std::vector<std::string> result;
+
+    std::string::const_iterator it = str.begin();
+    std::string::const_iterator end = str.end();
+    std::string::const_iterator previousIt = it;
+    while (it != end)
+    {
+        if (*it != ' ')
+        {
+            ++it;
+        }
+        else
+        {
+            if (previousIt != it)
+            {
+                result.emplace_back(previousIt, it);
+            }
+            // TODO: do I need a variant where I keep the separator?
+            //result.emplace_back(1, *it);
+            ++it;
+            previousIt = it;
+        }
+    }
+    if (previousIt != it)
+    {
+        result.emplace_back(previousIt, it);
+    }
+    
+    return result;
+}
+
 void ASCII::ToLowerCase(std::string& str)
 {
     for (char& c : str)
@@ -30,6 +63,28 @@ void ASCII::ToUpperCase(std::string& str)
         {
             c = 'A' + c - 'a';
         }
+    }
+}
+
+void ASCII::Trim(std::string& str)
+{
+    if (!str.empty())
+    {
+        std::string::const_iterator begin = str.begin();
+        std::string::const_iterator it = str.begin();
+        std::string::const_iterator end = str.end();
+        while ((it != end) && (*it == ' '))
+        {
+            ++it;
+        }
+        std::string::const_iterator startIt = it;
+        it = end;
+        while ((it != startIt) && (*(it - 1) == ' '))
+        {
+            --it;
+        }
+        str.erase(it, end);
+        str.erase(begin, startIt);
     }
 }
 
