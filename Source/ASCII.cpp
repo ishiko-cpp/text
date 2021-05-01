aad337fadedd561d421c2bb090b5c60005d32a99
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-01 15:56:32 +0100

    Implement and test ASCII::GetLines (#17)

--- Source/ASCII.cpp
+++ Source/ASCII.cpp
@@ -11,6 +11,47 @@ namespace Ishiko
 namespace Text
 {
 
+std::vector<std::string> ASCII::GetLines(const std::string& str)
+{
+    std::vector<std::string> result;
+
+    std::string::const_iterator it = str.begin();
+    std::string::const_iterator end = str.end();
+    std::string::const_iterator previousIt = it;
+    while (it != end)
+    {
+        if ((*it != '\r') && (*it != '\n'))
+        {
+            ++it;
+        }
+        else
+        {
+            result.emplace_back(previousIt, it);
+            if (*it == '\r')
+            {
+                ++it;
+                if ((it != end) && (*it == '\n'))
+                {
+                    ++it;
+                }
+            }
+            else
+            {
+                // TODO: do I need a variant where I keep the separator?
+                //result.emplace_back(1, *it);
+                ++it;
+            }
+            previousIt = it;
+        }
+    }
+    if (previousIt != it)
+    {
+        result.emplace_back(previousIt, it);
+    }
+
+    return result;
+}
+
 std::vector<std::string> ASCII::Split(const std::string& str)
 {
     std::vector<std::string> result;
