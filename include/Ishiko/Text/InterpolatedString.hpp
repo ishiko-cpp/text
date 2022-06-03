dce1839cf1a8b5be37626e9eb80490bba3204cc5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-03 13:31:36 +0100

    Implement InterpolatedString (#79)

---
+++ include/Ishiko/Text/InterpolatedString.hpp
@@ -0,0 +1,54 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/text/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TEXT_INTERPOLATEDSTRING_HPP_
+#define _ISHIKO_CPP_TEXT_INTERPOLATEDSTRING_HPP_
+
+#include <boost/utility/string_view.hpp>
+#include <Ishiko/Errors.hpp>
+#include <map>
+#include <string>
+
+namespace Ishiko
+{
+
+// TODO: these strings follow the bash syntax, do I need to support other types?
+class InterpolatedString
+{
+public:
+    class Callbacks
+    {
+    public:
+        virtual ~Callbacks() = default;
+
+        // TODO: it is up to the callback to decide whether to return an error when a variable is not found or not.
+        virtual void expand(boost::string_view variable, std::string& result, Error& error) const = 0;
+    };
+
+    class MapCallbacks : public Callbacks
+    {
+    public:
+        MapCallbacks(const std::map<std::string, std::string>& map, bool allowMissing);
+
+        void expand(boost::string_view variable, std::string& result, Error& error) const override;
+
+    private:
+        const std::map<std::string, std::string>& m_map;
+        bool m_allowMissing;
+    };
+
+    InterpolatedString();
+    InterpolatedString(std::string str);
+
+    std::string expand(const Callbacks& callbacks, Error& error) const;
+
+private:
+    std::string m_string;
+};
+
+}
+
+#endif
---
+++ tests/src/InterpolatedStringTests.hpp
@@ -0,0 +1,31 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/text/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TEXT_TESTS_INTERPOLATEDSTRINGTESTS_HPP_
+#define _ISHIKO_CPP_TEXT_TESTS_INTERPOLATEDSTRINGTESTS_HPP_
+
+#include <Ishiko/TestFramework/Core.hpp>
+
+class InterpolatedStringTests : public Ishiko::TestSequence
+{
+public:
+    InterpolatedStringTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context);
+
+private:
+    static void ConstructorTest1(Ishiko::Test& test);
+    static void ConstructorTest2(Ishiko::Test& test);
+    static void ExpandTest1(Ishiko::Test& test);
+    static void ExpandTest2(Ishiko::Test& test);
+    static void ExpandTest3(Ishiko::Test& test);
+    static void ExpandTest4(Ishiko::Test& test);
+    static void ExpandTest5(Ishiko::Test& test);
+    static void ExpandTest6(Ishiko::Test& test);
+    static void ExpandTest7(Ishiko::Test& test);
+    static void ExpandTest8(Ishiko::Test& test);
+    static void ExpandErrorTest1(Ishiko::Test& test);
+};
+
+#endif
