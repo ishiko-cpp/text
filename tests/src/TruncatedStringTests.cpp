9cd195930ce723189a991452409978bed1dc7660
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-06-12 11:10:13 +0100

    Add test files

---
+++ tests/src/TruncatedStringTests.cpp
@@ -0,0 +1,19 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/text/blob/main/LICENSE.txt
+*/
+
+#include "TruncatedStringTests.hpp"
+
+using namespace Ishiko;
+
+TruncatedStringTests::TruncatedStringTests(const TestNumber& number, const TestContext& context)
+    : TestSequence(number, "TruncatedString tests", context)
+{
+    append<HeapAllocationErrorsTest>("Constructor test 1", ConstructorTest1);
+}
+
+void TruncatedStringTests::ConstructorTest1(Test& test)
+{
+}
---
+++ tests/src/TruncatedStringTests.hpp
@@ -0,0 +1,21 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/text/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TEXT_TESTS_TRUNCATEDSTRINGTESTS_HPP_
+#define _ISHIKO_CPP_TEXT_TESTS_TRUNCATEDSTRINGTESTS_HPP_
+
+#include <Ishiko/TestFramework/Core.hpp>
+
+class TruncatedStringTests : public Ishiko::TestSequence
+{
+public:
+    TruncatedStringTests(const Ishiko::TestNumber& number, const Ishiko::TestContext& context);
+
+private:
+    static void ConstructorTest1(Ishiko::Test& test);
+};
+
+#endif
