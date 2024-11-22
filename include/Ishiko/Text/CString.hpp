46de70416e7f56c06bddb17fc9a3304e922ddd39
Xavier Leclercq <xavier@leclercq.dev>
2024-11-22 18:50:29 +0000

    Refactoring (#119)

--- include/Ishiko/Text/CString.hpp
+++ include/Ishiko/Text/CString.hpp
@@ -1,28 +1,23 @@
-/*
-    Copyright (c) 2020-2022 Xavier Leclercq
-    Released under the MIT License
-    See https://github.com/ishiko-cpp/text/blob/main/LICENSE.txt
-*/
+// SPDX-FileCopyrightText: 2000-2024 Xavier Leclercq
+// SPDX-License-Identifier: BSL-1.0
 
-#ifndef _ISHIKO_CPP_TEXT_CSTRING_HPP_
-#define _ISHIKO_CPP_TEXT_CSTRING_HPP_
+#ifndef GUARD_ISHIKO_CPP_TEXT_CSTRING_HPP
+#define GUARD_ISHIKO_CPP_TEXT_CSTRING_HPP
 
 #include <string>
 
 namespace Ishiko
 {
-
-class CString
-{
-public:
-    static char* Duplicate(const char* str);
-    static std::string Substring(const char* str, size_t begin);
-    static std::string Substring(const char* str, size_t begin, size_t end);
-    static size_t Find(const char* str, const char* substring);
-    static bool StartsWith(const char* str, const char* prefix);
-    static bool EndsWith(const char* str, const char* suffix);
-};
-
+    class CString
+    {
+    public:
+        static char* Duplicate(const char* str);
+        static std::string Substring(const char* str, size_t begin);
+        static std::string Substring(const char* str, size_t begin, size_t end);
+        static size_t Find(const char* str, const char* substring);
+        static bool StartsWith(const char* str, const char* prefix);
+        static bool EndsWith(const char* str, const char* suffix);
+    };
 }
 
 #endif
